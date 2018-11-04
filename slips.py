e3a8b5c3262b8670692fa52281718f4ab1e5c55f
Thomas Kiley <thomas.kiley@diffblue.com>
2018-11-04 16:29:19 +0000

    Adding basic main file

--- slips.py
+++ slips.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-import slips_lib
+from slips_lib.run_checks import run_checks
+import sys
 
 if __name__ == '__main__':
-	print('Usage: slips program_to_check.py')
+	if len(sys.argv) != 2:
+		print('Usage: lips program_to_check.py')
+		exit(1)
+
+	file_path = sys.argv[1]
+
+	with open(sys.argv[1]) as source_file:
+		soure_code = source_file.read()
+
+		run_checks(file_path, soure_code)
---
+++ slips_lib/run_checks.py
@@ -0,0 +1,2 @@
+def run_checks(file_path, source_code):
+	print('checking ' + file_path)
